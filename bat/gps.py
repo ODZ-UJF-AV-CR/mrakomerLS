732fc91227f5b47b3df764332a9a6021d03c757e
kakl <kakl@astro.cz>
2018-12-24 11:48:30 +0100

    os date

--- bat/gps.py
+++ bat/gps.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python
+import time
 import serial
+import os
 
 com = serial.Serial('/dev/ttyACM0')
-while(True):
+
+while(int(time.strftime('%Y')) < 2016):
 	data = com.readline().split(',')
 	if (data[0]=='$GNRMC'):
 		print data[1], data[9]
+		date_str = str(data[1]) + ' ' + data[9])
+		os.system('date +"%H%M%S.00 %d%m%y" -s %s' % date_str)
+		
 
