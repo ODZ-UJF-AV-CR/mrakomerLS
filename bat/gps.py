c193149033a20498d3280673bf81246811920fa2
kakl <martin.kakona@gmail.com>
2018-12-25 00:39:00 +0000

    first virst working version of GPS time initiation

--- bat/gps.py
+++ bat/gps.py
@@ -10,17 +10,23 @@ while (True):
     print 'GPS is not ready'
     time.sleep(5)
     continue
-  finally:
+  else:
     break
 
 while(int(time.strftime('%Y')) < 2016):
-  data = com.readline().split(',')
-  #print data
-  if (data[0]=='$GNRMC'):
-    date_str = str(data[9])
-    cmd =  '20'+date_str[-2:] + date_str[2:4] + date_str[0:2]
-    date_str = str(data[1])
-    cmd2 = date_str.split('.')
-    cmd3 = cmd2[0]
-    cmd3 = cmd3[0:2] + ':' + cmd3[2:4] + ':' + cmd3[4:6]
-    os.system('./settime.sh \"%s\"' % (cmd + ' ' + cmd3))
+    try:
+	data = com.readline().split(',')
+    except:
+	print 'GPS is not ready'
+	time.sleep(5)
+	continue
+    else:
+	if (data[0]=='$GNRMC'):
+	    date_str = str(data[9])
+	    cmd =  '20'+date_str[-2:] + date_str[2:4] + date_str[0:2]
+	    date_str = str(data[1])
+	    cmd2 = date_str.split('.')
+	    cmd3 = cmd2[0]
+	    cmd3 = cmd3[0:2] + ':' + cmd3[2:4] + ':' + cmd3[4:6]
+	    print cmd, cmd3
+	    os.system('/home/odroid/git/mrakomerLS/bat/settime.sh \"%s\"' % (cmd + ' ' + cmd3))
--- bat/settime.sh
+++ bat/settime.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $1 $2
-date +'%Y%m%d %H:%M:%S' --set "$1 $2"
+sudo date +'%Y%m%d %H:%M:%S' --set "$1 $2"
